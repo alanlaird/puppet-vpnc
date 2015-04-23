f51d7f03c417ea64cc2c0ed1be402479d8a69ede
Alan Laird <alan.laird@iovation.com>
2015-04-23 16:12:52 -0700

    first pass at rhel6/7

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,11 +9,22 @@ class vpnc::params {
       $hasstatus  = true
     }
     'CentOS': {
-      $init_path = '/etc/init.d/'
-      $init_append = ''
-      $init_template = 'vpnc/rh_initscript.erb'
-      $hasrestart = false
-      $hasstatus  = false
+      case $operatingsystemmajrelease {
+        '6': {
+          $init_path = '/etc/init.d/'
+          $init_append = ''
+          $init_template = 'vpnc/rh_initscript.erb'
+          $hasrestart = false
+          $hasstatus  = false
+        }
+        '7': {
+          $init_path = '/usr/lib/systemd/system'
+          $init_append = ''
+          $init_template = 'vpnc/vpnc.service.erb'
+          $hasrestart = false
+          $hasstatus  = false
+        }
+      }
     }
     default: {
       $init_path = '/etc/init.d/'
